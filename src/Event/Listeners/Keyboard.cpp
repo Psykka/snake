0ce578b08f9bc36ff65b1a62cbf3849c982a1bf1
Psykka <36780964+Psykka@users.noreply.github.com>
2022-06-07 23:10:34 -0300

    using single file

--- src/Event/Listeners/Keyboard.cpp
+++
@@ -1,9 +0,0 @@
-#include "Keyboard.hpp"
-#include <stdio.h>
-
-using namespace Event;
-
-void Keyboard::update(SDL_Event e)
-{
-    printf("Foi");
-}
--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,69 @@
 #include "SDL.h"
-// #include "Event/EventHandler.hpp"
 
-int main(int argc, char *argv[])
+int table[25][25];
+
+void draw()
+{
+
+}
+
+int main(int argc, char *argv[]**/)
 {
     SDL_Window *window;
     SDL_Renderer *renderer;
     SDL_Surface *surface;
     SDL_Event event;
 
-    // Event::EventHandler eventHandler;
-
     bool inputThisFrame = false;
-    // eventHandler.load();
 
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    for (int i = 0; i < 25; i++)
+    {
+        for (int j = 0; j < 25; j++)
+        {
+            
+        }
+    }
+    
+    
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
         return 3;
     }
 
-    if (SDL_CreateWindowAndRenderer(500, 500, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+    if (SDL_CreateWindowAndRenderer(500, 500, SDL_WINDOW_RESIZABLE, &window, &renderer))
+    {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window and renderer: %s", SDL_GetError());
         return 3;
     }
 
-
-    while (true) {
+    while (true)
+    {
         SDL_PollEvent(&event);
-        if (event.type == SDL_QUIT) {
+        if (event.type == SDL_QUIT)
+        {
             break;
         }
 
         if (event.type == SDL_KEYDOWN && inputThisFrame == false)
         {
-            // eventHandler.notify("keyboard", event);
+            
         }
-        
-        
+
         SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
         SDL_RenderClear(renderer);
+
+        SDL_Rect rect;
+
+        rect.h = 20;
+        rect.w = 20;
+        rect.x = 0;
+        rect.y = 0;
+
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF);
+        SDL_RenderFillRect(renderer, &rect);
+
         SDL_RenderPresent(renderer);
     }
 
