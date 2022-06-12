1535e6de55d53e7869a8ec37d681b1e140081162
Psykka <36780964+Psykka@users.noreply.github.com>
2022-06-12 04:51:29 -0300

    basic moviments

--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,95 @@
 #include "SDL.h"
 #include "stdio.h"
-#include <tuple>
+#include <time.h>
+#include <math.h>
 
 #define SIZE 25
+#define PIXEL 20
+#define WINDOW SIZE * PIXEL
+#define FPS_CAP 120 // afeta a velocidade do jogo
 
-int game[SIZE][SIZE];
-
-enum Objects
+typedef struct
 {
-    WALL = 1,
-    HEAD,
-    BODY,
-    FOOD
-};
+    int x;
+    int y;
+} Position;
 
-void drawGame(SDL_Renderer *renderer)
+int tailSize = 0;
+
+int yv = 0;     // velocidade y
+int xv = PIXEL; // velocidade x
+
+Position tail[SIZE * SIZE];
+Position head = {20, 20};
+Position food;
+
+int constrain(int x, int low, int high)
 {
-    // for (int i = 0; i < 25; i++)
-    // {
-    // }
-    
+    if (x < low)
+    {
+        return low;
+    }
+    else if (high < x)
+    {
+        return high;
+    }
+    else
+        return x;
+}
+
+void drawPixel(SDL_Renderer *renderer, int x, int y, SDL_Color rgb)
+{
     SDL_Rect rect;
 
-    rect.h = 20;
-    rect.w = 20;
-    rect.x = 0;
-    rect.y = 20;
+    rect.h = PIXEL;
+    rect.w = PIXEL;
+    rect.x = x;
+    rect.y = y;
 
-    SDL_SetRenderDrawColor(renderer, 0x00, 0xFF, 0x00, 0xFF);
+    SDL_SetRenderDrawColor(renderer, rgb.r, rgb.g, rgb.b, rgb.a);
     SDL_RenderFillRect(renderer, &rect);
+}
+
+void createFood()
+{
+    srand(time(0));
+    food.x = floor(rand() % WINDOW);
+    food.y = floor(rand() % WINDOW);
+}
+
+void updateSpeed()
+{
+    head.x += xv;
+    head.y += yv;
+
+    head.x = constrain(head.x, 0, WINDOW - PIXEL);
+    head.y = constrain(head.y, 0, WINDOW - PIXEL);
+}
+
+void gameLoop(SDL_Renderer *renderer)
+{
+    updateSpeed();
+
+    // render food
+    drawPixel(renderer, food.x, food.y, {0xF4, 0x35, 0x7F, 0xFF});
+
+    // render player
+    drawPixel(renderer, head.x, head.y, {0xFF, 0xFF, 0xFF, 0xFF});
+
+    if (head.x == food.x && head.y == food.y)
+    {
+        tailSize++;
+        createFood();
+    }
 
     SDL_RenderPresent(renderer);
 }
 
 int main(int argc, char *argv[])
 {
-    SDL_Window *window;
-    SDL_Renderer *renderer;
-    SDL_Surface *surface;
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
     SDL_Event event;
-
-    bool inputThisFrame = false;
-
-    for (int i = 0; i < SIZE; i++)
-    {
-        game[0][i] = Objects::WALL;
-        game[SIZE - 1][i] = Objects::WALL;
-        game[i][0] = Objects::WALL;
-        game[i][SIZE - 1] = Objects::WALL;
-    }
-
-    // for (int i = 0; i < SIZE; i++)
-    // {
-    //     for (int j = 0; j < SIZE; j++)
-    //     {
-    //         printf("%i ", game[i][j]);
-    //     }
-    //     printf("\n");
-    // }
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
@@ -65,28 +97,59 @@ int main(int argc, char *argv[])
         return 3;
     }
 
-    if (SDL_CreateWindowAndRenderer(520, 500, SDL_WINDOW_RESIZABLE, &window, &renderer))
-    {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window and renderer: %s", SDL_GetError());
-        return 3;
-    }
+    window = SDL_CreateWindow("Snake", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW, WINDOW, 0);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+    
+    unsigned int a = SDL_GetTicks();
+    unsigned int b = SDL_GetTicks();
+    double delta = 0;
 
-    drawGame(renderer);
+    createFood();
 
     while (true)
     {
+        a = SDL_GetTicks();
+        delta += a - b;
+
         SDL_PollEvent(&event);
         if (event.type == SDL_QUIT)
         {
             break;
         }
 
+        if (event.type == SDL_KEYDOWN)
+        {
+            if (event.key.keysym.scancode == SDL_SCANCODE_UP)
+            {
+                xv = 0;
+                yv = -PIXEL;
+            }
+            else if (event.key.keysym.scancode == SDL_SCANCODE_LEFT)
+            {
+                xv = -PIXEL;
+                yv = 0;
+            }
+            else if (event.key.keysym.scancode == SDL_SCANCODE_DOWN)
+            {
+                xv = 0;
+                yv = PIXEL;
+            }
+            else if (event.key.keysym.scancode == SDL_SCANCODE_RIGHT)
+            {
+                xv = PIXEL;
+                yv = 0;
+            }
+        }
 
-        // if (event.type == SDL_KEYDOWN && inputThisFrame == false)
-        // {
-        // }
+        if (delta > 1000 / FPS_CAP)
+        {
+            gameLoop(renderer);
+            delta = 0;
+        }
 
-        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+        b = SDL_GetTicks();
+
+        SDL_SetRenderDrawColor(renderer, 0x38, 0x38, 0x38, 0xFF);
         SDL_RenderClear(renderer);
     }
 
